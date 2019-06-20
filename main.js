34489cd20d7badf98e1909f56330f656f66454d7
Vadim Sirenko <vitalievichkresh@gmail.com>
2019-06-20 17:58:47 +0300

    added chacked type

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 'use strict';
 
 function daysInYear(year) {
-  if (/^\d{4}$/.test(year)) {
-    if(year % 4 === 0) {
-        return 366;
+  if (typeof year === 'number' && /^\d{4}$/.test(year)) {
+    if (year % 4 === 0) {
+      return 366;
     }
     return 365;
   }
@@ -15,4 +15,4 @@ daysInYear('2019');
 daysInYear(2020);
 daysInYear(2020.2);
 daysInYear(2100);
-daysInYear({year: 2100});
+daysInYear({ year: 2100 });
